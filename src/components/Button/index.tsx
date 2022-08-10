a44be167f24e5dfc3fff3bc285bf31baf2e22096
Natasha de Matos Oliveira <natashamo@uninove.br>
2022-08-10 16:54:02 -0700

    Sintaxe do classe component e function component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Button extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <button>Botão</button>
+    );
+  }
+}
+
+export default Button;
---
+++ src/components/List/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Lista() {
+    return (
+        <aside></aside>
+    );
+}
+
+export default Lista;
