dc05f55f96b46c50a680d25e9a43f134b465d13e
Natasha de Matos Oliveira <natashamo@uninove.br>
2022-08-10 18:57:59 -0700

    Add state para os inputs do formulário

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import style from "./Form.module.scss";
 
-class Form extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <form className={style.newTask}>
-        <div className={style.inputWrapper}>
-          <label htmlFor="task">Adicione um novo estudo</label>
-          <input
-            type="text"
-            name="task"
-            id="task"
-            placeholder="O que você quer estudar"
-            required
-          />
-        </div>
-        <div className={style.inputWrapper}>
-          <label htmlFor="duration">Duração</label>
-          <input
-            type="time"
-            step="1"
-            name="duration"
-            id="duration"
-            min="00:00:00"
-            max="01:30:00"
-            required
-          />
-        </div>
-        <Button>Adicionar</Button>
-      </form>
-    );
-  }
+function Form() {
+  const [state, setSate] = useState({
+    task: "",
+    duration: "00:00",
+  });
+  const addTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(state);
+  } 
+  return (
+    <form className={style.newTask} onSubmit={addTask}>
+      <div className={style.inputWrapper}>
+        <label htmlFor="task">Adicione um novo estudo</label>
+        <input
+          type="text"
+          name="task"
+          value={state.task}
+          onChange={(e) => setSate({...state, task: e.target.value})}
+          id="task"
+          placeholder="O que você quer estudar"
+          required
+        />
+      </div>
+      <div className={style.inputWrapper}>
+        <label htmlFor="duration">Duração</label>
+        <input
+          type="time"
+          step="1"
+          name="duration"
+          value={state.duration}
+          onChange={(e) => setSate({ ...state, duration: e.target.value })}
+          id="duration"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+      </div>
+      <Button>Adicionar</Button>
+    </form>
+  );
 }
 
 export default Form;
