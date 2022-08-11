c79ff61e4e5ec20867573b168bc14a706b1ff06e
Natasha de Matos Oliveira <natashamo@uninove.br>
2022-08-10 18:57:16 -0700

    Add state para a lista de tarefas

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 import style from "./style.module.scss";
 
 function Lista() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: "React",
       duration: "02:00:00",
@@ -16,10 +16,16 @@ function Lista() {
       task: "typeScript",
       duration: "02:30:00",
     },
-  ];
+  ]);
   return (
     <aside className={style.tasksList}>
-      <h2>Atividades do dia</h2>
+      <h2
+        onClick={() =>
+          (setTasks([...tasks, { task: "State React", duration: "00:30:00" }]))
+        }
+      >
+        Atividades do dia
+      </h2>
       <ul>
         {tasks.map((item, index) => (
           <Item key={index} {...item} />
